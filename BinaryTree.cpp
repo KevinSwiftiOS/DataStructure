919d7d02722259a39c9e810ab667343a668549e6
KevinSwift <KevinSwiftiOS@gmail.com>
2017-04-10 14:45:27 +0800

    层次遍历的追加

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -35,6 +35,31 @@ void PostOrder(Nodetree ptree){
         cout<<ptree->c;
     }
 }
+struct queue{
+    Nodetree key;
+};
+//层次遍历 用到了队列的知识 类似于广搜 如果左孩子存在 左孩子就入队列 右孩子存在 右孩子就入队列
+void LevelOrder(Nodetree ptree){
+    int rear = -1;
+    int front = -1;
+    queue q[10000];
+    if(!ptree)
+        return;
+    q[++rear].key = ptree;
+    while(front < rear){
+        if(ptree){
+            ptree = q[++front].key;
+            cout<<ptree -> c;
+            if(ptree -> left)
+                q[++rear].key = ptree->left;
+            if(ptree->right)
+                q[++rear].key = ptree->right;
+        }else{
+            break;
+        }
+    }
+    
+}
 //二叉树的创建 以递归方式实现的先序创建二叉树
 Nodetree createTreeProOrder() {
     BiTreeNode *ptree = NULL;
